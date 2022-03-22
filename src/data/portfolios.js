63139aee9360f9acd372c281dbfc5ec32bee4685
KumarGudipalli <kumargudipalli97@gmail.com>
2022-03-22 20:39:16 +0530

    updated

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -2,35 +2,63 @@ export const portfolios = {
   Web: [
     {
       type: "featured",
-      name: "Medplus-Mart",
-      image: require("./image/medplusmart.png"),
+      name: "twitter ",
+      image: require("./image/twitter.png"),
       description:
-        "MedPlus is an Indian pharmacy retailer ",
-      demo: "https://medplusmartclone.000webhostapp.com/",
-      source: "https://github.com/KumarGudipalli/medplusmart.com",
-      tools: [  "HTML", "CSS", "javaScript"],
+      "Twitter is a social media site, and its primary purpose is to connect people and allow people to share their thoughts with a big audience.",
+      demo: "https://twitter-project-work.netlify.app/",
+      source: "https://github.com/KumarGudipalli/twitter-clone",
+      tools: ["Axios", "React" ,"React-router-dom","Firebase", "styled-Components"],
     },
+    {
+      type: "featured",
+      name: "Tesla-Clone",
+      image: require("./image/tesla-clone.png"),
+      description:
+        `Tesla 's mission is to accelerate the world 's transition to sustainable energy`,
+      demo: "https://tesla-clone-six-brown.vercel.app/",
+      source: "https://github.com/KumarGudipalli/tesla-Clone",
+      tools: [  "React","styled-components","firebase","Redux"],
+    },
+    {
+      type: "featured",
+      name: "Todo-list ",
+      image: require("./image/Todo-list.png"),
+      description: "It’s a list of tasks you need to complete or things that you want to do",
+      demo: "https://tod-list-app.herokuapp.com/",
+      source: "https://github.com/KumarGudipalli/Todo-list",
+      tools: ["React","Redux"],
+    },
+    // {
+    //   type: "featured",
+    //   name: "POS-Store",
+    //   image: require("./image/Pos-store.png"),
+    //   description:"",
+    //   demo: "",
+    //   source: "",
+    //   tools: ["Axios", "React" ,"MongoDb","express","Redux"],
+    // },
+
     {
       type: "featured",
       name: "CultFit ",
       image: require("./image/cult-fit.png"),
-      description:
-        "health and fitness company offering digital and offline experiences across fitness, nutrition, and mental well-being.",
+      description:"health and fitness company offering digital and offline experiences across fitness, nutrition, and mental well-being.",
       demo: "https://cult-fit-clone-ruddy.vercel.app/",
       source: "https://github.com/KumarGudipalli/cult-fit-clone",
       tools: ["Axios", "HTML", "CSS", "MongoDB" , "JavaScript"],
     },
   ],
- /*  Others: [
-    {
-      type: "featured",
-      name: "DevTop",
-      image: require("./image/devtop.jpg"),
-      description:
-        "Cross-platform desktop tool for the developers, does some useful job and make life easy",
-      demo: "https://zonayedpca.github.io/DevTop",
-      source: "https://github.com/zonayedpca/DevTop",
-      tools: ["electron", "react"],
-    },
-  ], */
+  //  Others: [
+  //   {
+  //     type: "featured",
+  //     name: "DevTop",
+  //     image: require("./image/devtop.jpg"),
+  //     description:
+  //       "Cross-platform desktop tool for the developers, does some useful job and make life easy",
+  //     demo: "https://zonayedpca.github.io/DevTop",
+  //     source: "https://github.com/zonayedpca/DevTop",
+  //     tools: ["electron", "react"],
+  //   },
+  // ], 
 }
